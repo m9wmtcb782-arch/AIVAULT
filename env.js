67d94d174cfc2e556bdec12400332b64b4800ee4
m9wmtcb782-arch <m9wmtcb782@privaterelay.appleid.com>
2026-09-04 11:42:30 +0800

    Create env.js

---
+++ env.js
@@ -0,0 +1,7 @@
+window.AIVAULT_CONFIG = {
+  supabaseUrl: "https://clcddygkaaqqtsbswgdf.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsY2RkeWdrYWFxcXRzYnN3Z2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MzUwNjQsImV4cCI6MjEwMjMxMTA2NH0.kYg6h7n74CtbiIjNjZ2xxJj16SV42INZVzQ9dLNUfKE",
+  useLocalEngine: false,
+  worldSize: 24,
+  tickSeconds: 30
+};
