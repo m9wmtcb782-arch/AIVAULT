48720e35971963040f9ce923ea0ea41e0ee7a18d
m9wmtcb782-arch <m9wmtcb782@privaterelay.appleid.com>
2026-09-05 13:40:09 +0800

    Delete js directory

--- js/boot.js
+++
@@ -1,9 +0,0 @@
-function qs(id) { return document.getElementById(id); }
-function go(href) { location.href = href; }
-document.addEventListener("DOMContentLoaded", () => {
-  if (window.FKAudio) FKAudio.boot();
-});
-async function guarded(fn) {
-  try { await fn(); }
-  catch (e) { console.error(e); alert(e.message || String(e)); }
-}
